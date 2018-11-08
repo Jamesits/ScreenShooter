aa70402666d12456b0e0d8dc69be039e7fcfe201
James Swineson <github@public.swineson.me>
2018-11-08 14:11:43 +0800

    Correctly process lazy loading pages

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # ScreenShooter
 
+[![Build Status](https://dev.azure.com/nekomimiswitch/General/_apis/build/status/ScreenShooter)](https://dev.azure.com/nekomimiswitch/General/_build/latest?definitionId=26)
+
 An automatic screenshot mailer service.
 
 > Some links have died, but they are still alive;
--- ScreenShooter.Actuator/HeadlessChromeActuator.cs
+++ ScreenShooter.Actuator/HeadlessChromeActuator.cs
@@ -37,14 +37,49 @@ namespace ScreenShooter.Actuator
                 Height = windowHeight
             });
             Logger.Debug($"Navigate to {url}");
-            await _page.GoToAsync(url);
+            try
+            {
+                await _page.GoToAsync(
+                    url,
+                    timeout: 30000,
+                    waitUntil: new[] { WaitUntilNavigation.Networkidle0 }
+                );
+            }
+            catch (WaitTaskTimeoutException)
+            {
+                // TODO: time exceeded
+                Logger.Warn("Page loading time exceeded.");
+            }
+            
+            Logger.Debug("Trying to load lazy-loading elements");
+            // https://www.screenshotbin.com/blog/handling-lazy-loaded-webpages-puppeteer
+            var bodyHandle = await _page.QuerySelectorAsync("body");
+            var boundingBox = await bodyHandle.BoundingBoxAsync();
+            var viewportHeight = _page.Viewport.Height;
+            var viewportIncr = 0;
+
+            // scroll down
+            while (viewportIncr + viewportHeight < boundingBox.Height)
+            {
+                await _page.EvaluateExpressionAsync($"window.scrollBy(0, {viewportHeight})");
+                await _page.WaitForTimeoutAsync(2000);
+                viewportIncr += viewportHeight;
+            }
+
+            // scroll up
+            await _page.EvaluateExpressionAsync("window.scrollTo(0, 0)");
+            await _page.WaitForTimeoutAsync(10000);
+
             Logger.Debug("Exit CreateSession()");
         }
 
         public async Task CaptureImage(string fileName)
         {
             Logger.Debug($"Take screenshot to {fileName}");
-            await _page.ScreenshotAsync(fileName);
+            await _page.ScreenshotAsync(fileName, new ScreenshotOptions()
+            {
+                FullPage = true
+            });
         }
 
         public async Task CapturePdf(string fileName)
