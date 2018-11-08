dce2f850cb50d63ab632e738e9eaf0d89088cb7b
James Swineson <github@public.swineson.me>
2018-11-08 10:28:00 +0800

    Add project files.

---
+++ ScreenShooter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ScreenShooter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
