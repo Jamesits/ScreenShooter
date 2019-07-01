0194d41f759c2f9f84732a3cb12ed3bc947fe6a4
James Swineson <github@public.swineson.me>
2019-07-02 00:21:05 +0800

    set up chrome executable permission under Linux

--- ScreenShooter.Actuator/HeadlessChromeActuator.cs
+++ ScreenShooter.Actuator/HeadlessChromeActuator.cs
@@ -5,11 +5,31 @@ using NLog;
 using PuppeteerSharp;
 using PuppeteerSharp.Media;
 using ScreenShooter.Helper;
+using System.Runtime.InteropServices;
+using RuntimeInformation = System.Runtime.InteropServices.RuntimeInformation;
 
 namespace ScreenShooter.Actuator
 {
     public class HeadlessChromeActuator : IActuator
     {
+        [DllImport("libc", SetLastError = true)]
+        private static extern int chmod(string pathname, int mode);
+
+        // user permissions
+        const int S_IRUSR = 0x100;
+        const int S_IWUSR = 0x80;
+        const int S_IXUSR = 0x40;
+
+        // group permission
+        const int S_IRGRP = 0x20;
+        const int S_IWGRP = 0x10;
+        const int S_IXGRP = 0x8;
+
+        // other permissions
+        const int S_IROTH = 0x4;
+        const int S_IWOTH = 0x2;
+        const int S_IXOTH = 0x1;
+
         UserRequestType[] IActuator.Capability => new [] { UserRequestType.Pdf, UserRequestType.Png };
         
         private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
@@ -246,6 +266,18 @@ namespace ScreenShooter.Actuator
 
             Logger.Debug("Download browser");
             AsyncHelper.RunSync(() => browserFetcher.DownloadAsync(BrowserFetcher.DefaultRevision));
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                var executablePath = browserFetcher.GetExecutablePath(BrowserFetcher.DefaultRevision);
+                const int _0755 =
+                S_IRUSR | S_IXUSR | S_IWUSR
+                | S_IRGRP | S_IXGRP
+                | S_IROTH | S_IXOTH;
+                chmod(executablePath, _0755);
+            }
+
+            Logger.Debug("Browser downloaded");
         }
     }
 }
